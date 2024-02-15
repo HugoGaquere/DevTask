fbd455dbcec4bb8947c6c150863f505f6d7bd274
Hugo <hugo.gaquere@gmail.com>
2024-02-15 19:44:14 +0100

    fix github action

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@
 dotnet publish --configuration Release --runtime linux-x64 --self-contained true DevTask/DevTask.csproj -o ./publish
 
 # Copy the install.sh file to the publish directory
-cp install.sh ./publish
+cp ./scripts/install.sh ./publish
 
 # Create the installer
 makeself ./publish devtask.sh "DevTask installer" ./install.sh
